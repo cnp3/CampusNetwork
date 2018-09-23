81889b74e951595b47bfcaa80daeb32499ea2a31
Mathieu Jadin <mathjadin@gmail.com>
2018-09-23 22:19:41 +0200

    Mount puppet /puppetmodule and /templates along with /etc

--- _node_utils.sh
+++ _node_utils.sh
@@ -8,6 +8,18 @@ function node_cfg {
     echo "${BDIR}/${CONFIGDIR}/$1"
 }
 
+# Get the config template directory
+function template_cfg {
+    # Make the template dir if non-existent
+    mkdir -p "${BDIR}/${CONFIGDIR}/templates" > /dev/null &> /dev/null
+    echo "${BDIR}/${CONFIGDIR}/templates"
+}
+
+# Get the puppet module directory
+function puppet_modules {
+    mkdir -p "${BDIR}/${CONFIGDIR}/puppetmodules" > /dev/null &> /dev/null
+    echo "${BDIR}/${CONFIGDIR}/puppetmodules"
+}
 # Get a node /run directory
 # $1: node name
 function node_rundir {
@@ -18,5 +30,7 @@ function node_rundir {
 # $1: node name
 # $@: command terms
 function node_exec_command {
-    ip netns exec "$1" bash -c "mount --bind $(node_cfg $1) /etc && mount --bind $(node_rundir $1) /run && ${@:2}"
+    ip netns exec "$1" bash -c "mount --bind $(node_cfg $1) /etc && mkdir -p /templates \
+     && mount --bind $(template_cfg) /templates && mkdir -p /puppetmodules \
+     && mount --bind $(puppet_modules) /puppetmodules && mount --bind $(node_rundir $1) /run && ${@:2}"
 }
